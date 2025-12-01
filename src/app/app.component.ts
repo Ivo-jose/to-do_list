47ef4033e9219e56bd476875667d2a4b9e929d76
Ivo Goncalves <ivojmgs19@gmail.com>
2025-12-01 11:16:40 -0300

    Container and button styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<p>Teste</p><router-outlet />`
+  template: `<router-outlet />`
 })
 export class AppComponent {
 }
