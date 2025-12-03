92e5fb743acdf3e466fb8211671d6ae7178dd4c0
Ivo Goncalves <ivojmgs19@gmail.com>
2025-12-03 10:55:04 -0300

    Input List Component Item created

---
+++ src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-list-item.component.html',
+  styleUrl: './input-list-item.component.scss'
+})
+export class InputListItemComponent {
+
+}
