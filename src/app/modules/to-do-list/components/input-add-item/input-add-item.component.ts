26f33763c49fedda709ed472cc04d545d0e2b8b8
Ivo Goncalves <ivojmgs19@gmail.com>
2025-12-03 08:55:53 -0300

    Persisting data in localStorage and transferring data between components

--- src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Output, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
+import { NgClass } from "@angular/common";
 
 // Interfaces
 import { IListItems } from '../../interfaces/IListItems.interface';
 
+
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss'
 })
@@ -15,6 +17,7 @@ export class InputAddItemComponent {
   private cdr = inject(ChangeDetectorRef);
   @Output() public outputAddListItems = new EventEmitter<IListItems>();
   @ViewChild("inputText") public inputText!: ElementRef;
+  @Input({ required: true }) public inputListItems: IListItems[] = [];
 
   public focusAndAddItem(value: string) {
     if (value) {
@@ -30,7 +33,7 @@ export class InputAddItemComponent {
         checked:  false,
         value
       });
-      
+
       return this.inputText.nativeElement.focus();
     }
   }
--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -3,15 +3,29 @@ import { Component, signal } from '@angular/core';
 // Components
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 
+// Interfaces
+import { IListItems } from '../../interfaces/IListItems.interface';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent],
+  imports: [CommonModule, InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
 
+  private setListItems = signal<IListItems[]>(this.parseItems());
+  public getListItems = this.setListItems.asReadonly();
   public addItem = signal<boolean>(true);
 
+  public getInputAndAddItem(value: IListItems) {
+    localStorage.setItem('@my-list', JSON.stringify([...this.setListItems(), value]));
+    return this.setListItems.set(this.parseItems());
+  }
+
+  private parseItems() {
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
 }
